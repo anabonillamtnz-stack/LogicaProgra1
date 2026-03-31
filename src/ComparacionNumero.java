3ca4ce6df6c160375a7a882e151190535eadd922
Ana Bonilla <ana.bonilla.mtnz@gmail.com>
2026-03-31 17:34:59 -0600

    Correción de print

--- src/ComparacionNumero.java
+++ src/ComparacionNumero.java
@@ -12,21 +12,23 @@ public class ComparacionNumero {
         System.out.println("Introduzca su primer numero (puede ser en decimales): ");
         double num1 = scanner.nextDouble();
 
-        System.out.println("Introduzca su primer numero (puede ser en decimales): ");
+        System.out.println("Introduzca su segundo número (puede ser en decimales): ");
         double num2 = scanner.nextDouble();
 
-        System.out.println("Introduzca su primer numero (puede ser en decimales): ");
+        System.out.println("Introduzca su tercer número (puede ser en decimales): ");
         double num3 = scanner.nextDouble();
 
         double [] numbers = {num1, num2, num3};
         Arrays.sort(numbers);
         System.out.println("Tus números de menor a mayor son: " + Arrays.toString(numbers));
 
-        System.out.println("Tus números de menor a mayor son: " + "[" + numbers[2] + ", " + numbers[1] + ", " + numbers[0]+ "]");
+        System.out.println("Tus números de mayor a menor son: " + "[" + numbers[2] + ", " + numbers[1] + ", " + numbers[0]+ "]");
 
         System.out.println("Tu número de enmedio es: " + numbers[1]);
 
         System.out.println("Sus números son iguales: " + (num1 == num2 && num2 == num3) );
+
+        System.out.println("¡El programa ha terminado!");
 
 
 
